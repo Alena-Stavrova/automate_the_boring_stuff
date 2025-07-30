580971e204e7355077e741a595bdcbe24ff705a5
Alena Stavrova <33433876+Alena-Stavrova@users.noreply.github.com>
2025-07-30 17:42:02 +0300

    Add files via upload
    Added an updated (simplified) file, 1st project from the 3rd version of the book

--- collatz.py
+++ collatz.py
@@ -1,21 +1,25 @@
+#Write a function named collatz() that has one parameter named number.
+#If number is even, then collatz() should print number // 2 and return this value.
+#If number is odd, then collatz() should print and return 3 * number + 1.
+
 def collatz(number):
     if number % 2 == 0:
-        return(number // 2)
-
-    elif number % 2 == 1:
+        return(number// 2)
+    else:
         return(3 * number + 1)
 
-print('Enter an integer')
+#Then, write a program that lets the user enter an integer and that keeps calling collatz() on that number until the function returns the value 1.
 
+#Add try and except statements to the previous project to detect whether the user entered a non-integer string.
+#Normally, the int() function will raise a ValueError error if it is passed a non-integer string, as in int('puppy').
+#In the except clause, print a message to the user saying they must enter an integer.
+
+user_number = input()
 try:
-    usersNumber = int(input())
-    while usersNumber != 1:
-        print(collatz(usersNumber))
-        usersNumber = collatz(usersNumber)
-        if usersNumber == 1:
-            break
+    while int(user_number) != 1:
+        user_number = collatz(int(user_number))
+        print(user_number)
 except ValueError:
-    print('You must have entered a string. Please enter an integer.')
-
+        print('Please enter an integer')
 
 
