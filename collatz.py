808db1b08dfe77928c8f3e9b7f7f7c65a1f1cf84
Alena Stavrova <33433876+Alena-Stavrova@users.noreply.github.com>
2025-07-31 16:07:22 +0300

    Update collatz.py
    Added chapter

--- collatz.py
+++ collatz.py
@@ -1,3 +1,5 @@
+#Chapter 4 (3rd ed)
+
 #Write a function named collatz() that has one parameter named number.
 #If number is even, then collatz() should print number // 2 and return this value.
 #If number is odd, then collatz() should print and return 3 * number + 1.
